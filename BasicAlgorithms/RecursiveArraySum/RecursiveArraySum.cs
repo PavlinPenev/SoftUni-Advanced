ccca4715a6b3b98f78cdd75c5fb237902d77835e
PavlinPenev <86779216+PavlinPenev@users.noreply.github.com>
2021-09-15 23:07:27 +0300

    Update

---
+++ BasicAlgorithms/RecursiveArraySum/RecursiveArraySum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursiveArraySum
+{
+    class RecursiveArraySum
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int currentIdx = 0;
+            int sum = 0;
+            Console.WriteLine(Sum(array, currentIdx, sum));
+        }
+
+        private static int Sum(int[] array, int idx, int sum)
+        {
+            if (idx == array.Length)
+            {
+                return sum;
+            }
+            sum += array[idx];
+            idx++;
+            
+            return Sum(array, idx, sum);
+        }
+    }
+}
---
+++ BasicAlgorithms/RecursiveFactorial/RecursiveFactorial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RecursiveFactorial
+{
+    class RecursiveFactorial
+    {
+        static void Main()
+        {
+            double n = double.Parse(Console.ReadLine());
+            double result = 1;
+            Console.WriteLine(Factorial(n, result));
+        }
+
+        static double Factorial(double x, double result)
+        {
+
+            if (x <= 1)
+            {
+                return result;
+            }
+
+            result *= x;
+            x--;
+            return Factorial(x, result);
+        }
+    }
+}
