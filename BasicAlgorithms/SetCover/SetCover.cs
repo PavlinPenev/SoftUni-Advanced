3fe39895b0a2eea6f93d5b1c4776048b4407e5e1
PavlinPenev <86779216+PavlinPenev@users.noreply.github.com>
2021-09-18 21:15:13 +0300

    Update

---
+++ BasicAlgorithms/SetCover/SetCover.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SetCover
+{
+    public static void Main(string[] args)
+    {
+        int[] universe = new[] { 1, 3, 5, 7, 9, 11, 20, 30, 40 };
+        int[][] sets = new[]
+        {
+                new[] { 20 },
+                new[] { 1, 5, 20, 30 },
+                new[] { 3, 7, 20, 30, 40 },
+                new[] { 9, 30 },
+                new[] { 11, 20, 30, 40 },
+                new[] { 3, 7, 40 }
+            };
+
+        List<int[]> selectedSets = ChooseSets(sets.ToList(), universe.ToList());
+        Console.WriteLine($"Sets to take ({selectedSets.Count}):");
+
+        foreach (int[] set in selectedSets)
+        {
+            Console.WriteLine($"{{ {string.Join(", ", set)} }}");
+        }
+    }
+
+    public static List<int[]> ChooseSets(IList<int[]> sets, IList<int> universe)
+    {
+        var selectedSets = new List<int[]>();
+        while (universe.Count > 0)
+        {
+            var currentSet = sets.OrderByDescending(s => s.Count(universe.Contains)).First();
+            selectedSets.Add(currentSet);
+            sets.Remove(currentSet);
+            foreach (var i in currentSet)
+            {
+                if (universe.Contains(i))
+                {
+                    universe.Remove(i);
+                }
+            }
+        }
+
+        return selectedSets;
+    }
+}
---
+++ BasicAlgorithms/SumOfCoins/SumOfCoins.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SumOfCoins
+{
+    public static void Main(string[] args)
+    {
+        var availableCoins = new[] { 1, 2, 5, 10, 20, 50 };
+        var targetSum = 923;
+
+        try
+        {
+            var selectedCoins = ChooseCoins(availableCoins, targetSum);
+            Console.WriteLine($"Number of coins to take: {selectedCoins.Values.Sum()}");
+            foreach (var selectedCoin in selectedCoins)
+            {
+                Console.WriteLine($"{selectedCoin.Value} coin(s) with value {selectedCoin.Key}");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        
+    }
+
+    public static Dictionary<int, int> ChooseCoins(IList<int> coins, int targetSum)
+    {
+        Dictionary<int, int> result = new Dictionary<int, int>();
+
+        int currentSum = 0;
+        int sumLeft = targetSum;
+        coins = coins.OrderByDescending(x => x).ToList();
+
+        for (int i = 0; i < coins.Count; i++)
+        {
+            int currentCoinValue = coins[i];
+            int currentCoinCount = sumLeft / currentCoinValue;
+
+            if (currentSum + currentCoinValue * currentCoinCount > targetSum)
+            {
+                continue;
+            }
+
+            sumLeft -= currentCoinValue * currentCoinCount;
+            currentSum += currentCoinValue * currentCoinCount;
+
+            if (currentCoinCount != 0)
+            {
+                result.Add(currentCoinValue, currentCoinCount);
+            }
+        }
+
+        if (sumLeft != 0)
+        {
+            throw new InvalidOperationException("Error");
+        }
+
+        return result;
+    }
+}
