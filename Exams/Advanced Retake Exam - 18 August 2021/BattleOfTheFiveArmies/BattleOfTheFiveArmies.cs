95979c389e6a325bdb5dd88be43c73d4a83dcf16
PavlinPenev <86779216+PavlinPenev@users.noreply.github.com>
2021-10-17 17:37:21 +0300

    Exams

---
+++ Exams/Advanced Retake Exam - 18 August 2021/BattleOfTheFiveArmies/BattleOfTheFiveArmies.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace BattleOfTheFiveArmies
+{
+    class BattleOfTheFiveArmies
+    {
+        static void Main(string[] args)
+        {
+            int armor = int.Parse(Console.ReadLine());
+            int rows = int.Parse(Console.ReadLine());
+            char[][] field = new char[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                field[i] = Console.ReadLine().ToCharArray();
+            }
+            int[] armyPosition = FindArmyPosition(field);
+
+            bool throneReached = false;
+
+            while (armor > 0 && !throneReached)
+            {
+                string[] moveSpawnCommands = Console.ReadLine().Split();
+                string moveCommand = moveSpawnCommands[0];
+                int spawnRowIdx = int.Parse(moveSpawnCommands[1]);
+                int spawnColIdx = int.Parse(moveSpawnCommands[2]);
+                field[spawnRowIdx][spawnColIdx] = 'O';
+                field[armyPosition[0]][armyPosition[1]] = '-';
+                switch (moveCommand)
+                {
+                    case "up":
+                        if (ValidatePosition(field, armyPosition[0] - 1, armyPosition[1]))
+                        {
+                            armyPosition[0]--;
+                        }
+                        break;
+                    case "down":
+                        if (ValidatePosition(field, armyPosition[0] + 1, armyPosition[1]))
+                        {
+                            armyPosition[0]++;
+                        }
+                        break;
+                    case "left":
+                        if (ValidatePosition(field, armyPosition[0], armyPosition[1] - 1))
+                        {
+                            armyPosition[1]--;
+                        }
+                        break;
+                    case "right":
+                        if (ValidatePosition(field, armyPosition[0], armyPosition[1] + 1))
+                        {
+                            armyPosition[1]++;
+                        }
+                        break;
+                }
+                armor--;
+                if (field[armyPosition[0]][armyPosition[1]] == 'M')
+                {
+                    throneReached = true;
+                    field[armyPosition[0]][armyPosition[1]] = '-';
+                    break;
+                }
+
+                if (field[armyPosition[0]][armyPosition[1]] == 'O')
+                {
+                    armor -= 2;
+                }
+                if (armor <= 0)
+                {
+                    field[armyPosition[0]][armyPosition[1]] = 'X';
+                    break;
+                }
+                field[armyPosition[0]][armyPosition[1]] = 'A';
+            }
+            Console.WriteLine(armor <= 0 ? $"The army was defeated at {armyPosition[0]};{armyPosition[1]}." : $"The army managed to free the Middle World! Armor left: {armor}");
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                Console.WriteLine(field[i]);
+            }
+        }
+
+        private static bool ValidatePosition(char[][] field, int armyPositionRow, int armyPositionCol)
+        {
+            return (armyPositionRow >= 0 && armyPositionRow < field.GetLength(0)) && (armyPositionCol >= 0 && armyPositionCol < field[armyPositionRow].Length);
+        }
+
+        private static int[] FindArmyPosition(char[][] field)
+        {
+            int[] indexes = new int[2];
+            bool isFound = false;
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                for (int j = 0; j < field[i].Length; j++)
+                {
+                    if (field[i][j] == 'A')
+                    {
+                        indexes[0] = i;
+                        indexes[1] = j;
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (isFound)
+                {
+                    break;
+                }
+            }
+            return indexes;
+        }
+    }
+}
---
+++ Exams/Advanced Retake Exam - 18 August 2021/BirthdayCelebration/BirthdayCelebration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BirthdayCelebration
+{
+    class BirthdayCelebration
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> guests = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).Reverse());
+            Stack<int> plates = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int wastedFood = 0;
+
+            while (guests.Count > 0 && plates.Count > 0)
+            {
+                if (plates.Peek() >= guests.Peek())
+                {
+                    wastedFood += Math.Abs(plates.Peek() - guests.Peek());
+                    guests.Pop();
+                }
+                else
+                {
+                    guests.Push(guests.Pop() - plates.Peek());
+                }
+                plates.Pop();
+            }
+            Console.WriteLine(guests.Count == 0 ? $"Plates: {string.Join(" ", plates)}" : $"Guests: {string.Join(" ", guests)}");
+            Console.WriteLine($"Wasted grams of food: {wastedFood}");
+        }
+    }
+}
