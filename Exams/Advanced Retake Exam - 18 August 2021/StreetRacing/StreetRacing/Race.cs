44a1f2dc2dbd19af4fdb94e1bebd1de6519d6550
PavlinPenev <86779216+PavlinPenev@users.noreply.github.com>
2021-10-17 18:10:36 +0300

    Exams

---
+++ Exams/Advanced Retake Exam - 18 August 2021/StreetRacing/StreetRacing/Race.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Race
+    {
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            Name = name;
+            Type = type;
+            Laps = laps;
+            Capacity = capacity;
+            MaxHorsePower = maxHorsePower;
+            Participants = new List<Car>();
+        }
+
+        public List<Car> Participants { get; set; }
+        public int Count => Participants.Count;
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Laps { get; set; }
+        public int Capacity { get; set; }
+        public int MaxHorsePower { get; set; }
+
+        public void Add(Car car)
+        {
+            if (!Participants.Any(p => p.LicensePlate == car.LicensePlate) && Participants.Count + 1 <= Capacity && car.HorsePower <= MaxHorsePower)
+            {
+                Participants.Add(car);
+            }
+        }
+
+        public bool Remove(string licensePlate)
+        {
+            Car carToRemove = Participants.FirstOrDefault(c => c.LicensePlate == licensePlate);
+            if (carToRemove == null)
+            {
+                return false;
+            }
+            Participants.Remove(carToRemove);
+            return true;
+        }
+
+        public Car FindParticipant(string licensePlate)
+        {
+            return Participants.FirstOrDefault(c => c.LicensePlate == licensePlate);
+        }
+
+        public Car GetMostPowerfulCar()
+        {
+            return Participants.OrderByDescending(c => c.HorsePower).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Race: {Name} - Type: {Type} (Laps: {Laps})");
+            foreach (var participant in Participants)
+            {
+                sb.AppendLine(participant.ToString());
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Exams/Advanced Retake Exam - 18 August 2021/StreetRacing/StreetRacing/StartUp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace StreetRacing
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Sample Code Usage:
+
+            //Initialize Race
+            Race race = new Race("RockPort Race", "Sprint", 1, 4, 150);
+
+            //Initialize Car
+            Car car = new Car("BMW", "320ci", "NFS2005", 150, 1450);
+
+            //Print car
+            Console.WriteLine(car.ToString());
+
+            //Make: BMW
+            //Model: 320ci
+            //License Plate: NFS2005
+            //Horse Power: 150
+            //Weight: 1450
+
+            //Add car
+            race.Add(car);
+
+            //Remove car
+            Console.WriteLine(race.Remove("NFS2005")); // True
+
+            Car carOne = new Car("BMW", "320cd", "NFS2007", 150, 1350);
+            Car carTwo = new Car("Audi", "A3", "NFS2004", 131, 1300);
+            //Add cars
+            race.Add(carOne);
+            race.Add(carTwo);
+
+            //GetMostPowerfulCar
+            Console.WriteLine(race.GetMostPowerfulCar());
+
+            //Make: BMW
+            //Model: 320cd
+            //License Plate: NFS2007
+            //Horse Power: 150
+            //Weight: 1350
+
+
+            //Print Race report
+            Console.WriteLine(race.Report());
+
+            //Race: RockPort Race - Type: Sprint (Laps: 1)
+            //Make: BMW
+            //Model: 320cd
+            //License Plate: NFS2007
+            //Horse Power: 150
+            //Weight: 1350
+            //Make: Audi
+            //Model: A3
+            //License Plate: NFS2004
+            //Horse Power: 131
+            //Weight: 1300
+
+        }
+    }
+}
